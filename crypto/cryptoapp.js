d2d29f37f84b536d9de483b50974484acf22e8a3
ahrum noh <sydneyroseenglish@gmail.com>
2021-10-14 13:30:15 +1100

    Live Time inserted into

--- crypto/cryptoapp.js
+++ crypto/cryptoapp.js
@@ -42,3 +42,8 @@ fetch(`${proxyUrl}${baseUrl}`, {
   .catch((error) => {
     console.log(error);
   });
+
+
+// Current Time //
+var d = new Date();
+document.getElementById('currentTime').innerHTML = d.toLocaleString();
---
+++ public/javascript/time.js
@@ -0,0 +1,3 @@
+// Current Time //
+var d = new Date();
+document.getElementById('currentTime').innerHTML = d.toLocaleString();
